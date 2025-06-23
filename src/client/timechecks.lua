36d989612ae701b6b521d49bf5eafd72c23c8f91
MrNewb <47620135+MrNewb@users.noreply.github.com>
2025-06-22 22:56:44 -0400

    locales added

--- src/client/timechecks.lua
+++ src/client/timechecks.lua
@@ -2,9 +2,7 @@ function VerifyDayTime(storeName)
     local currenthour = GetClockHours()
     if Config.PawnShops[storeName] and Config.PawnShops[storeName].StoreHours then
         local storeHours = Config.PawnShops[storeName].StoreHours
-        if storeHours.open <= currenthour and storeHours.close >= currenthour then
-            return true
-        end
+        if storeHours.open <= currenthour and storeHours.close >= currenthour then return true end
         return false
     end
 
--- src/server/main.lua
+++ src/server/main.lua
@@ -1,9 +1,9 @@
 RegisterNetEvent("MrNewbPawn_V2:Server:SellPawn", function(item, amount)
     local src = source
     local verified = Config.PawnItems[item]
-    if not verified then return Bridge.Notify.SendNotify(src, locale("Warnings.InvalidItem"), "error", 6000) end
+    if not verified then return NotifyPlayer(src, locale("Warnings.InvalidItem"), "error", 6000) end
     local itemCheck = Bridge.Inventory.GetItemCount(src, item)
-    if itemCheck < amount then return Bridge.Notify.SendNotify(src, locale("Warnings.DoNotHave"), "error", 6000) end
+    if itemCheck < amount then return NotifyPlayer(src, locale("Warnings.DoNotHave"), "error", 6000) end
     Bridge.Inventory.RemoveItem(src, item, amount)
     local price = verified * amount
     Bridge.Framework.AddAccountBalance(src, "money", price)
@@ -13,16 +13,16 @@ end)
 RegisterNetEvent('MrNewbPawn_V2:Server:Scrapping', function(itemName, amount)
     local src = source
     local meltableItems = Config.MeltableItems[itemName]
-    if not meltableItems then return Bridge.Notify.SendNotify(src, locale("Warnings.InvalidItem"), "error", 6000) end
+    if not meltableItems then return NotifyPlayer(src, locale("Warnings.InvalidItem"), "error", 6000) end
     local itemCheck = Bridge.Inventory.GetItemCount(src, itemName)
-    if itemCheck < amount then return Bridge.Notify.SendNotify(src, locale("Warnings.DoNotHave"), "error", 6000) end
+    if itemCheck < amount then return NotifyPlayer(src, locale("Warnings.DoNotHave"), "error", 6000) end
     Bridge.Inventory.RemoveItem(src, itemName, amount)
     for _, reward in pairs(meltableItems) do
         local rewardCount = reward.count * amount
         Bridge.Inventory.AddItem(src, reward.itemName, rewardCount)
         DebugInfo("Player " .. src .. " received " .. rewardCount .. " of item " .. reward.itemName)
     end
-    Bridge.Notify.SendNotify(src, locale("Smelting.SmeltingSuccess"), "success", 6000)
+    NotifyPlayer(src, locale("Smelting.SmeltingSuccess"), "success", 6000)
 end)
 
 CreateThread(function()
